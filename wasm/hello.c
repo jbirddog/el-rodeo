c18c42d0b78a2528181fade12e2d4c6d4bf326b8
Jon Herron <jon.herron@yahoo.com>
2024-09-25 08:12:34 -0400

    WIP

--- wasm/hello.c
+++ wasm/hello.c
@@ -1,49 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-extern char **environ;
 
 int main(int argc, char **argv) {
-	printf("Content-Type: text/html\n\n");
-	printf("<html><head><title>Hello</title><body><h1>hello</h1>\n");
-
-	printf("<h3>argv</h3>\n");
-	printf("<ul>\n");
-
-	for (int i = 0; i < argc; ++i) {
-		printf("<li>%s</li>\n", argv[i]);
-	}
-	
-	printf("</ul>\n");
-
-	printf("<h3>cwd</h3>\n");
-
-	char cwd[4096];
-	if (getcwd(cwd, sizeof(cwd)) != NULL) {
-		printf("<p>%s</p>\n", cwd);
-	}
-	
-	printf("<h3>env</h3>\n");
-
-	const char *envs[] = {
-		"SCRIPT_FILENAME",
-		NULL,
-	};
-
-	for (int i = 0;; ++i)  {
-		const char *key = envs[i];
-		if (!key) break;
-		
-		const char *val = getenv(key);
-		printf("<p>%s: %s</p>\n", key, val);
-	}
-
-	char **env = environ;
-	for (; *env; ++env) {
-		printf("%s<br />\n", *env);
-	}
-
-	printf("</body></html>\n\n");
+	printf("Content-Type: application/json\n\n");
+	printf("{\"status\": \"ok\"}");
 	return 0;
 }
