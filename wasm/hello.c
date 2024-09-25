f02a392682fa555761060813b5b589481a3e3568
Jon Herron <jon.herron@yahoo.com>
2024-09-25 08:10:16 -0400

    WIP

--- wasm/hello.c
+++ wasm/hello.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+
+extern char **environ;
 
 int main(int argc, char **argv) {
 	printf("Content-Type: text/html\n\n");
@@ -21,6 +24,26 @@ int main(int argc, char **argv) {
 		printf("<p>%s</p>\n", cwd);
 	}
 	
+	printf("<h3>env</h3>\n");
+
+	const char *envs[] = {
+		"SCRIPT_FILENAME",
+		NULL,
+	};
+
+	for (int i = 0;; ++i)  {
+		const char *key = envs[i];
+		if (!key) break;
+		
+		const char *val = getenv(key);
+		printf("<p>%s: %s</p>\n", key, val);
+	}
+
+	char **env = environ;
+	for (; *env; ++env) {
+		printf("%s<br />\n", *env);
+	}
+
 	printf("</body></html>\n\n");
 	return 0;
 }
