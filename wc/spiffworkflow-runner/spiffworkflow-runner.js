0b6de59841854bf844d67b25cbb773f328711a47
Jon Herron <jon.herron@yahoo.com>
2024-10-24 10:12:36 -0400

    WIP

--- wc/spiffworkflow-runner/spiffworkflow-runner.js
+++ wc/spiffworkflow-runner/spiffworkflow-runner.js
@@ -1,25 +1,24 @@
-// local
-const workflowRunner = 'http://localhost:8100'
-//const workflowApiKey = '31200470-da18-48f9-8ba1-6225be674c33'
-
-// prod
-//const workflowRunner = 'https://john-dark-meadow-5957.fly.dev';
-//const workflowApiKey = 'e5116459-c1d2-4dbe-aaea-7911a0c115bc';
 
 export class SpiffWorkflowRunner extends HTMLElement {
   static observedAttributes = ["apiKey"]
+  apiKey = null;
+  completed = false;
+  pendingTasks = [];
+  runner = 'http://localhost:8100'
+  state = {};
+  status = null;
   
   constructor() {
     super()
   }
 
   async connectedCallback() {
-    const apiKey = this.getAttribute("apiKey");
+    this.apiKey = this.getAttribute("apiKey");
     const shadow = this.attachShadow({ mode: "open" });
     const wrapper = document.createElement("div");
     
     wrapper.setAttribute("class", "wrapper");
-    wrapper.textContent = `Loading... ${apiKey}`;
+    wrapper.textContent = `Loading... ${this.apiKey}`;
 
     const style = document.createElement("style");
     style.textContent = `
@@ -33,36 +32,34 @@ export class SpiffWorkflowRunner extends HTMLElement {
     shadow.appendChild(style);
     shadow.appendChild(wrapper);
 
-    const workflowState = {};
     const additionalBody = {};
 
-
-    const resp = await fetch(`${workflowRunner}/v0/do/${apiKey}`, {
+    const resp = await fetch(`${this.runner}/v0/do/${this.apiKey}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        ...workflowState,
+        ...this.state,
         ...additionalBody,
       }),
     });
 
     const json = await resp.json();
+
     console.log(json);
+
+    this.status = json.status ?? 'error';
+    this.completed = json.completed ?? false;
+    this.state = json.state ?? {};
+    this.pendingTasks = json.pending_tasks ?? [];
+    
+    wrapper.textContent = JSON.stringify(this.pendingTasks);
   }
 
-  disconnectedCallback() {
-    console.log("Custom element removed from page.");
-  }
-
-  adoptedCallback() {
-    console.log("Custom element moved to new page.");
-  }
-
-  attributeChangedCallback(name, oldValue, _newValue) {
+  attributeChangedCallback(name, oldValue, newValue) {
     console.log(`Attribute ${name} has changed.`);
-  }  
+  }
 }
 
 customElements.define('spiffworkflow-runner', SpiffWorkflowRunner)
